79646b8ac221c16c9375c84f535fa1f317626b88
ilovecheese28s <semzatogi@gmail.com>
2025-10-19 01:31:56 +0400

    seventh commit

---
+++ day 6/input.py
@@ -0,0 +1,12 @@
+name = input("Enter your name: ")
+
+print(name)
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a second number: "))
+print(num1 + num2)
+
+# ჩემი აზრით ციფრები ინახება სტრინგის სახით ამიტომ მას როგორც სტრინგ ამატებს იმის ნაცვლად რომ
+# მათემატიკურად დააჯამო 
+
+# კონკადინაცია = print("vashli" + "5")
