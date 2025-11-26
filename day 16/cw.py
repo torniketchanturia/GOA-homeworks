f02b0efc889d95647f80bd51199655b36271889c
qweqwe1qq <semzatogi@gmail.com>
2025-11-26 23:15:07 +0400

    site navigation

--- day 16/cw.py
+++ day 16/cw.py
@@ -4,14 +4,14 @@ while num <= 200:
     print(num)
     num += 1
 
-num = 0
+num = 2
 
-while num <= 20:
+while num < 20:
     print(num)
     num += 2
 
 username = input("Input a name: ")
 my_name = "Toko"
 while username != my_name:
-    username = input("That's different from my name:'c ")
+    username = input("That's different from my name:'c Try again: ")
 print("That's also my name!!:D")
